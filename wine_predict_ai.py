c240d1de00f0fb58f79409e7650c5495e038cb99
404-Abhishekk <abhishekkrishna404@gmail.com>
2025-08-10 17:18:29 +0530

    Delete wine_predict_ai.py

--- wine_predict_ai.py
+++
@@ -1,5 +0,0 @@
-import joblib as jb
-import numpy as np
-model = jb.load('redwine_model.pkl')
-result = model.predict(np.array([[7.4, 0.7, 0.0, 1.9, 0.076, 11.0, 34.0, 0.9978, 3.51, 0.56, 9.4]]))
-print("Predicted Quality=", result)
